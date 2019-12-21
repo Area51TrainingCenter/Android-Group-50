89db84cc12361d81dfacccd9465e8aaa771cd5f7
Johann Jara Sanchez <johannfjs@gmail.com>
2019-12-21 15:34:54 -0500

    clase

--- Mod2/Clase06/app/src/main/java/com/jcodee/clase06/MainActivity.java
+++ Mod2/Clase06/app/src/main/java/com/jcodee/clase06/MainActivity.java
@@ -1,11 +1,14 @@
 package com.jcodee.clase06;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 
@@ -15,6 +18,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvDatos;
+    private FrameLayout flCargando;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rvDatos = findViewById(R.id.rvDatos);
+        flCargando = findViewById(R.id.flCargando);
+
+        flCargando.setVisibility(View.VISIBLE);
 
         RetrofitServicios servicios =
                 RetrofitConfiguracion.obtenerConfiguracion()
@@ -38,11 +45,12 @@ public class MainActivity extends AppCompatActivity {
                         new UsuarioAdapter(MainActivity.this, response.body());
                 rvDatos.setLayoutManager(new LinearLayoutManager(MainActivity.this));
                 rvDatos.setAdapter(adapter);
+                flCargando.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<ArrayList<Usuario>> call, Throwable t) {
-
+                flCargando.setVisibility(View.GONE);
             }
         });
     }
